c0476229475568d52af228a4bc34d884d6a2f1af
Hope Gochnour <hope.gochnour@gmail.com>
2021-04-26 09:56:32 -0600

    Add urban_search class, restructure salaries controller

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -1,8 +1,12 @@
-class APi::V1::SalariesController < ApplicationController
+class Api::V1::SalariesController < ApplicationController
   def index
     conn = Faraday.new(url: 'https://api.teleport.org')
+    ua_id = "slug:#{params[:destination]}"
 
-    ua_id = conn.get("/api/urban_areas/slug:#{params[:destination]}/")
+    response = conn.get("/api/urban_areas/#{ua_id}/salaries")
+    response_parsed = JSON.parse(response.body, symbolize_names: true)
+
+    UrbanSearch.new(response_parsed)
     binding.pry
   end
 end
---
+++ app/poros/urban_search.rb
@@ -0,0 +1,20 @@
+class UrbanSearch
+  attr_reader :destination, :forecast, :salaries
+
+  def initialize(salary_data, weather_data, search_term)
+    @destination = search_term
+    @forecast = compile_weather_data(weather_data)
+    @salaries = compile_salary_data(salary_data)
+  end
+
+  def compile_weather_data(weather_data)
+    {
+      summary: weather_data.current_weather[:conditions],
+      temperature: "#{weather.current_weather[:temperature]} F"
+    }
+  end
+
+  def compile_salary_data(salary_data)
+    
+  end
+end

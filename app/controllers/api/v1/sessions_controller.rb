88e56c37971aa2ed7dd75dc476486943e9eff9c9
Hope Gochnour <hope.gochnour@gmail.com>
2021-04-26 16:51:14 -0600

    move read_body method to application_controller to clean up code

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,15 +5,4 @@ class Api::V1::SessionsController < ApplicationController
     user.authenticate(user_params[:password])
     render json: UsersSerializer.new(user).serialized_json
   end
-
-  private
-
-  def read_body
-    body = request.body.read
-    if body == ""
-      Hash.new
-    else
-      JSON.parse(body, symbolize_names: true)
-    end
-  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  def read_body
+    body = request.body.read
+    if body == ""
+      Hash.new
+    else
+      JSON.parse(body, symbolize_names: true)
+    end
+  end
 end

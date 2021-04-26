0a67d59f2c7958bf2b740a96b039bf11d9bacd45
Hope Gochnour <hope.gochnour@gmail.com>
2021-04-26 10:47:23 -0600

    tiny refactor in salaries controller

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::SalariesController < ApplicationController
   def index
+    location = params[:destination]
     conn = Faraday.new(url: 'https://api.teleport.org')
-    ua_id = "slug:#{params[:destination]}"
+    ua_id = "slug:#{location}"
 
     response = conn.get("/api/urban_areas/#{ua_id}/salaries")
     salaries_search = JSON.parse(response.body, symbolize_names: true)
 
-    coordinates = MapsService.fetch_coordinates(params[:destination])
+    coordinates = MapsService.fetch_coordinates(location)
     lat = coordinates[:results][0][:locations][0][:latLng][:lat]
     long = coordinates[:results][0][:locations][0][:latLng][:lng]
 
@@ -14,9 +15,7 @@ class Api::V1::SalariesController < ApplicationController
 
     forecast = Forecast.new(weather)
 
-    urban = UrbanSearch.new(salaries_search[:salaries], forecast, params[:destination])
+    urban = UrbanSearch.new(salaries_search[:salaries], forecast, location)
     render json: SalariesSerializer.new(urban)
-
-    # binding.pry
   end
 end

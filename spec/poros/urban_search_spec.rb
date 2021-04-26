8657bfb104a2ee4a7cab393d94115b00c307a396
Hope Gochnour <hope.gochnour@gmail.com>
2021-04-26 11:47:06 -0600

    Add fixture file for salary search and create urban_search spec

--- spec/poros/urban_search_spec.rb
+++ spec/poros/urban_search_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 describe UrbanSearch, type: :class do
   before do
-    @urban = SalariesFacade.find_salary_information("dallas")
+    VCR.use_cassette('weather_for_denver') do
+      weather = WeatherService.fetch_weather_for_location("denver,co")
+      forecast = Forecast.new(weather)
+      job_search = File.read('./spec/fixtures/chicago_salary_search.json')
+      parsed_salaries = JSON.parse(job_search, symbolize_names: true)
+      @urban = UrbanSearch.new(parsed_salaries, forecast, "denver")
+    end
   end
 
   it 'destination' do
-    expect(@urban.destination).to eq("dallas")
+    expect(@urban.destination).to eq("denver")
   end
 
   it 'forecast' do
